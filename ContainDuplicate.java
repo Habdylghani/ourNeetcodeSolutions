6891100026b953d30559d624162ccad3dddb0266
Abdelghani HOUBBADI <houbbadi.abdelghani@gmail.com>
2022-08-19 15:41:41 -0500

    ContainDuplicate 217 leetCode

---
+++ ContainDuplicate.java
@@ -0,0 +1,19 @@
+import java.util.*;
+//19/08/2022
+class ContainDuplicate {
+    public static boolean containDuplicate(int[] nums) {
+        HashSet<Integer> mySet = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!mySet.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 2, 2, 5, 4, 8 };
+        System.out.println(containDuplicate(arr));
+    }
+
+}
