9e1890bcdd3e2e5c9c4fd4731b21e4c4233cb625
Abdelghani HOUBBADI <houbbadi.abdelghani@gmail.com>
2022-09-02 00:02:04 -0500

    238leetcode ProductExceptSelf

---
+++ ProductExceptSelf.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = nums[0];
+        suffix[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+            suffix[n - 1 - i] = suffix[n - i] * nums[n - 1 - i];
+        }
+        System.out.println(Arrays.toString(prefix));
+        System.out.println(Arrays.toString(suffix));
+
+        int[] res = new int[n];
+        res[0] = suffix[1];
+        res[n - 1] = prefix[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            res[i] = prefix[i - 1] * suffix[i + 1];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        ProductExceptSelf p = new ProductExceptSelf();
+        System.out.println(Arrays.toString(p.productExceptSelf(nums)));
+    }
+}
